9448ade4f8db4bee19fcc226ceedeadf95d974c5
Papito <tetayculo25gmail.com>
2020-06-13 14:05:07 -0400

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 
 
 const clima = (city) => {
-  fetch(`http://localhost:3000/weather?address=${city}`).then((response) => {
+  fetch(`/weather?address=${city}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         console.log(data.error);
